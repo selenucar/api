168e6feb794c51ebf293130cb3ce14b581cb75ef
selenucar <slnucartx@gmail.com>
2021-09-15 18:15:26 -0500

    day02

---
+++ src/test/java/get_http_request_method/Get05.java
@@ -0,0 +1,19 @@
+package get_http_request_method;
+
+public class Get05 {
+
+    // on Postman --> https://restful-booker.herokuapp.com/booking?firstname=Mary&lastname=Ericsson
+    // ***query param: selecting a specific data
+    /*
+    Given
+        https://restful-booker.herokuapp.com/booking
+    When
+        User send a request to the URL
+    Then
+        Status code is 200
+    And
+        And Among the data there should be someone whose first name is "Mark" and last name is "Ericsson"
+    */
+
+
+}
